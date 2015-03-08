ba79327add20d6f4e694d4851b6d9d506e095625
Trystan Spangler <trystan.s@comcast.net>
2015-03-08 08:19:08 -0700

    initial commit

---
+++ test/runner_puncher/core_test.clj
@@ -0,0 +1,7 @@
+(ns runner-puncher.core-test
+  (:require [clojure.test :refer :all]
+            [runner-puncher.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
